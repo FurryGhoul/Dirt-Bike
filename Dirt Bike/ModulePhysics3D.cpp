54e830bf3700b3ca58cf7e4ed2d27cfc2e2b5fc4
MaxitoSama <marc.garcia.u@gmail.com>
2018-01-06 23:43:10 +0100

    Release

--- Dirt Bike/ModulePhysics3D.cpp
+++ Dirt Bike/ModulePhysics3D.cpp
@@ -115,7 +115,7 @@ update_status ModulePhysics3D::Update(float dt)
 	if(App->input->GetKey(SDL_SCANCODE_F1) == KEY_DOWN)
 		debug = !debug;
 
-	if(debug == true)
+	if(debug == false)
 	{
 		world->debugDrawWorld();
 
@@ -125,14 +125,6 @@ update_status ModulePhysics3D::Update(float dt)
 		{
 			item->data->Render();
 			item = item->next;
-		}
-
-		if(App->input->GetKey(SDL_SCANCODE_1) == KEY_DOWN)
-		{
-			Sphere s(1);
-			s.SetPos(App->camera->Position.x, App->camera->Position.y, App->camera->Position.z);
-			float force = 30.0f;
-			AddBody(s)->Push(-(App->camera->Z.x * force), -(App->camera->Z.y * force), -(App->camera->Z.z * force));
 		}
 	}
 
